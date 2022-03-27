89ac65cb0c859a9e63d492340ef9bd6c61f5e341
n.abbasi <nimaab1992@gmail.com>
2022-03-27 19:44:48 +0430

    feat: axios and vuetify and vue2 for client side

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,30 +1,16 @@
 <template>
-  <Login />
+  <v-app>
+    <v-main>
+      <router-view/>
+    </v-main>
+  </v-app>
 </template>
 
-
 <script>
-import Login from "./components/Login.vue";
+
 export default {
-components:{
-  Login
-}
-}
+  name: 'App',
+
+
+};
 </script>
-
-
-<style>
-*{
-  margin: 0;
-  padding: 0;
-}
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  width: 100%;
-  height: 100vh;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- client/src/main.js
+++ client/src/main.js
@@ -1,4 +1,12 @@
-import { createApp } from 'vue'
+import Vue from 'vue'
 import App from './App.vue'
-import './index.css'
-createApp(App).mount('#app')
+import router from './router'
+import vuetify from './plugins/vuetify'
+
+Vue.config.productionTip = false
+
+new Vue({
+  router,
+  vuetify,
+  render: h => h(App)
+}).$mount('#app')
