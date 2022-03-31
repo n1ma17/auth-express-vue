eec4ff2344c8d80c0843402fd95d84120286539e
n.abbasi <nimaab1992@gmail.com>
2022-03-31 16:38:15 +0430

    change client project to vue3 and quasar

--- client/src/main.js
+++ client/src/main.js
@@ -1,19 +1,9 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import vuetify from './plugins/vuetify'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
 
-import { library } from '@fortawesome/fontawesome-svg-core'
-/* import font awesome icon component */
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import { faArrowRightLong,faBarcode } from '@fortawesome/free-solid-svg-icons'
-library.add(faBarcode)
-library.add(faArrowRightLong)
-Vue.component('font-awesome-icon', FontAwesomeIcon)
-Vue.config.productionTip = false
-
-new Vue({
-  router,
-  vuetify,
-  render: h => h(App)
-}).$mount('#app')
+const app = createApp(App);
+app.use(store);
+app.use(router);
+app.mount("#app");
