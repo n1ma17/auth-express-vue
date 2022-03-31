b1a36aec6d9c25fea8ec0d9685d6503436866454
n.abbasi <nimaab1992@gmail.com>
2022-03-31 16:40:52 +0430

    add quasar to vue 3

--- client/src/main.js
+++ client/src/main.js
@@ -2,8 +2,10 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import { Quasar } from "quasar";
+import quasarUserOptions from "./quasar-user-options";
 
-const app = createApp(App);
+const app = createApp(App).use(Quasar, quasarUserOptions);
 app.use(store);
 app.use(router);
 app.mount("#app");
