e25ded33543c5982bfe6e21a14c0132abfa28659
n.abbasi <nimaab1992@gmail.com>
2022-04-03 18:16:13 +0430

    feat: cors dependencies

--- api/index.js
+++ api/index.js
@@ -1,10 +1,11 @@
 const express = require("express");
+const cors = require('cors');
 const app = express();
 const port = 5000;
 const jwt = require("jsonwebtoken");
 
 app.use(express.json());
-
+app.use(cors());
 const users = [
   {
     id: "1",
